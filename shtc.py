abe0439e68f361f98b71a0651a029134661a8863
06Days (Toby Cammock-Elliott) <63680371+06Days@users.noreply.github.com>
2026-04-03 13:42:43 +1300

    This code is very confusing.

--- shtc.py
+++ shtc.py
@@ -15,3 +15,47 @@ REGISTERS = {
     'SHTC3_NM_CD_READTH': 0x7866,
     'SHTC3_NM_CD_READRH': 0x58E0
 }
+
+class SHTC3:
+    def __init__(self, address: int = SHTC3_I2C_ADDRESS, bus_id: int=1):
+        self._address= address
+        self._bus = smbus.SMBus(bus_id)
+        self._reset()
+    # Check the checksum coming from the incoming data this helps with error detection
+    def _check_crc(self,data:list, length:int, checksum:int)-> bool:
+        crc= 0xFF
+        for byteCtr in range(0, length):
+            crc=crc^data[byteCtr]
+            for bit in range(0,8):
+                if crc & 0x80:
+                    crc= (crc <<1)^CRC_POLYNOMIAL
+                else:
+                    crc=crc << 1
+            crc &=0xFF
+        return crc==checksum
+    def _write_command(self,cmd_key: str) ->None:
+        cmd = REGISTERS[cmd_key]
+        self._bus.write_byte_data(self._address, cmd >>8, cmd & 0xFF)
+    def _reset(self) -> None:
+        self._write_command('SHTC3_SOFTWARE_RES')
+        time.sleep(0.01)
+    def wakeup(self) ->None:
+        try:
+            self._write_command('SHTC3_WAKEUP')
+            time.sleep(0.01)
+        except OSError:
+            pass
+    def sleep(self)->None:
+        self._write_command('SHTC3_SLEEP')
+        time.sleep(0.01)
+    def read_temperature_c(self)->float:
+        self.wakeup()
+        self._write_command('SHTC3_NM_CD_READTH')
+        time.sleep(0.02)
+        buffer=self._bus.read_ic2_block_date(self._address,0,3)
+        self.sleep(0.02)
+        if self._check_crc(buffer,2,buffer[2]):
+            raw=(buffer[0]<<8) | buffer[1]
+            return (raw * 175.0/65536.0) -45.0
+        return 0.0
+    # def read_humidity_relative(self)
