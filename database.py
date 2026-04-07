fc07548c5469b9e6ca7f6a8531a9d58272eec94b
06Days (Toby Cammock-Elliott) <63680371+06Days@users.noreply.github.com>
2026-04-07 14:59:15 +1200

    Better schema

--- database.py
+++ database.py
@@ -43,12 +43,12 @@ CREATE TABLE IF NOT EXISTS "SensorRecords" (
 	"Humidity"	REAL NOT NULL,
 	PRIMARY KEY("RecordID" AUTOINCREMENT)
 );
-INSERT INTO "EventTypes" VALUES (1,'Highest Temperature','The highest temperature recorded by the sensor');
-INSERT INTO "EventTypes" VALUES (2,'Lowest Temperature','The lowest temperature recorded by the sensor');
-INSERT INTO "EventTypes" VALUES (3,'Highest Pressure','The highest air pressure recorded by the sensor');
-INSERT INTO "EventTypes" VALUES (4,'Lowest Pressure','The lowest air pressure recorded by the sensor');
-INSERT INTO "EventTypes" VALUES (5,'Highest Humidity','The highest humidity recorded by the sensor');
-INSERT INTO "EventTypes" VALUES (6,'Lowest Humidity','The lowest humidity recorded by the sensor');
+INSERT OR IGNORE INTO "EventTypes" VALUES (1,'Highest Temperature','The highest temperature recorded by the sensor');
+INSERT OR IGNORE INTO "EventTypes" VALUES (2,'Lowest Temperature','The lowest temperature recorded by the sensor');
+INSERT OR IGNORE INTO "EventTypes" VALUES (3,'Highest Pressure','The highest air pressure recorded by the sensor');
+INSERT OR IGNORE INTO "EventTypes" VALUES (4,'Lowest Pressure','The lowest air pressure recorded by the sensor');
+INSERT OR IGNORE INTO "EventTypes" VALUES (5,'Highest Humidity','The highest humidity recorded by the sensor');
+INSERT OR IGNORE INTO "EventTypes" VALUES (6,'Lowest Humidity','The lowest humidity recorded by the sensor');
 COMMIT;
 """
 
